8b483584f54a69c838848a332de2070f9a17efb3
Prashantkaushik07 <160236438+Prashantkaushik07@users.noreply.github.com>
2025-05-06 01:15:48 +0530

    Latest Changes

---
+++ src/Components/OptimizeImage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const OptimizeImage = () => {
+  return (
+    <div className="section">
+      <h3>Optimize Image</h3>
+      <ul>
+        <li>Compress Image</li>
+        <li>Compress JPG</li>
+        <li>Compress PNG</li>
+        <li>Compress JPEG</li>
+        <li>Compress WEBP</li>
+        <li>Compress HEIC</li>
+        <li>Compress BMP</li>
+      </ul>
+    </div>
+  );
+};
+
+export default OptimizeImage;
